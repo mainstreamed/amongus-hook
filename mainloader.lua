dbe217ff8bd194e8ac99d03496a8e4349e15b825
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2025-03-23 07:46:58 +1000

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -68,7 +68,7 @@ statuslist.fallensurvival = {
 };
 statuslist.tridentsurvival = {
 	name 		= 'Trident Survival';
-	status 		= 'Down';
+	status 		= 'Undetected';
 	support 	= {'Wave'; 'AWP'; 'Synapse Z'; 'MacSploit'; 'Velocity'};
 };
 
