df043e2da7e52dec0984a3f269c051c4a94274a9
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2025-09-04 20:53:23 +1000

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -63,7 +63,7 @@ local statuslist = {};
 
 statuslist.fallensurvival = {
 	name 		= 'Fallen Survival';
-	status 		= 'Undetected';
+	status 		= 'Use at own risk';
 	support 	= {'Zenith'; 'Swift'; 'Wave'; 'Potassium'; 'Volcano'; };
 };
 statuslist.tridentsurvival = {
