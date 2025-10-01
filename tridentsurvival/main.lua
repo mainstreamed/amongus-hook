dad957af7fefe42dc2bc038e449b70b4367803b6
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2025-10-01 23:07:30 +1000

    Update main.lua

--- tridentsurvival/main.lua
+++ tridentsurvival/main.lua
@@ -1,40 +1,3 @@
-local check_run_on_actor = function()
-      if (identifyexecutor) then
-            local exec = identifyexecutor();
-            if (exec == 'AWP' or exec == 'Potassium' or exec == 'Seliware' or exec == 'Volcano') then
-                  return true;
-            end;
-      end;
-
-      local event = Instance.new('BindableEvent', game:GetService('CoreGui'));
-      event.Name = 'communicator';
-
-      local success = false;
-      local connection;
-      connection = event.Event:Connect(function(...)
-            if (... == 'recieved') then
-                  success = true;
-                  event:Destroy();
-                  connection:Disconnect();
-            end;
-      end);
-      run_on_actor(Instance.new('Actor'), [[
-            local event = game:GetService('CoreGui'):FindFirstChild('communicator');
-            if (not event) then
-                  return;
-            end;
-            event:Fire('recieved');
-      ]]);
-      task.wait();
-      if (not success) then
-            event:Destroy();
-            connection:Disconnect();
-            return false;
-      end;
-      return true;
-end;
-
-
 local players           = game:GetService('Players');
 local localplayer       = players.LocalPlayer;
 if (not localplayer) then
@@ -47,11 +10,27 @@ if (getgenv and getgenv().DEBUG_AMGHOOK) then
       source = 'getgenv().DEBUG_AMGHOOK = true;' .. source;
 end;
 
+-- Drawing Fix ( FUCK VOLCANO )
+source = [==[
+      if (not Drawing) then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/mainstreamed/amongus-hook/refs/heads/main/assets/trident/luaDrawing.lua"))();
+      end;
+]==] .. source;
+
+local executor = identifyexecutor and identifyexecutor() or 'Unknown';
+
+-- Main Load
 local fastflag = getfflag and getfflag('DebugRunParallelLuaOnMainThread');
 if (fastflag == 'true' or fastflag == 'True' or fastflag == true) then
       loadstring(source)();
       return;
-elseif (run_on_actor and check_run_on_actor()) then
+elseif (run_on_actor) then
+
+      local executors = { 'Volcano'; '' };
+      if (table.find(executors, executor)) then
+            return localplayer:Kick('Please use fflag - "#fflag-script" channel in discord');
+      end;
+
       local actor = getactors and getactors()[1] or localplayer:FindFirstChildWhichIsA('Actor', true);
       if (actor) then
             return run_on_actor(actor, source);
