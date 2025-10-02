6879521eabd98ce3a30182bb83e056e334b3608c
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2025-10-02 22:24:30 +1000

    Update espLibrary.lua

--- assets/trident/espLibrary.lua
+++ assets/trident/espLibrary.lua
@@ -328,7 +328,7 @@ do
 
             
             table.insert(self.connections, self.model.AncestryChanged:Connect(function(child, parent)
-                  if (child == entity.model and parent == nil) then
+                  if (child == self.model and parent == nil) then
                         return self:remove();
                   end;
             end));
