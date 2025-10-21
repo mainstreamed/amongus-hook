2af8e06dc9d077ee2f682ab689381dc69aca8e74
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2025-10-21 21:59:17 +1000

    Create supportedGames.lua

---
+++ supportedGames.lua
@@ -0,0 +1,43 @@
+local supportedGames    = {}
+
+supportedGames.FALLEN = {
+
+      placeIDs          = { 13800717766; 15479377118; 16849012343; };
+      gitPath           = 'fallensurvival';
+
+      gameName          = 'Fallen Survival';
+      status            = 'Undetected';
+      executors         = { 'Zenith'; 'Wave'; 'Swift'; };
+      customMessage     = {
+            ['Potassium']     = 'Use at own risk';
+            ['Volcano']       = 'Use at own risk';
+      };
+};
+supportedGames.TRIDENT = {
+
+      placeIDs          = { 13253735473; };
+      gitPath           = 'tridentsurvival';
+
+      gameName          = 'Trident Survival';
+      status            = 'Undetected';
+      executors         = { 'Zenith'; 'Wave'; 'Swift'; 'Velocity'; 'Potassium'; 'Seliware'; 'Volcano'; };
+      customMessage     = {
+
+            ['executor']      = 'custom message';
+      };
+};
+supportedGames.LONE = {
+
+      placeIDs          = { 13800223141; 139307005148921; };
+      gitPath           = 'lonesurvival';
+
+      gameName          = 'Lone Survival';
+      status            = 'Undetected';
+      executors         = { 'Zenith'; 'Wave'; 'Swift'; 'Velocity'; 'Potassium'; 'Seliware'; 'Volcano'; };
+      customMessage     = {
+
+            ['executor']      = 'custom message';
+      };
+};
+
+return supportedGames;
